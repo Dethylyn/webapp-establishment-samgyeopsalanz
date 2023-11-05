74c338639784b964f0cd6dc17a2fd1259e3d43e8
Dethylyn <dethylyn.b@gmail.com>
2023-11-06 02:17:49 +0800

    Reset env and add register button

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -18,7 +18,8 @@
         <div class="">
 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 my-5">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            {{-- class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" --}}
+            <div > 
                 {{-- <div class="p-6 text-gray-900 dark:text-gray-100"> --}}
 
                 <h3 class="text-center float-left m-5 text-2xl font-bold text-orange-50"> 
